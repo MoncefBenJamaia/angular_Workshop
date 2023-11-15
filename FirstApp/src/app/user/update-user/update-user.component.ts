3414aa473b23257f2ba1bb917f6b4bee8bb87aff
MoncefBenJamaia <mohamedmoncef.benjamaia@esprit.tn>
2023-11-15 20:07:09 +0100

    reactive_form

--- FirstApp/src/app/user/update-user/update-user.component.ts
+++ FirstApp/src/app/user/update-user/update-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from 'src/app/Core/Services/user.service';
 import { User } from 'src/app/Models/user';
@@ -12,36 +12,59 @@ import { User } from 'src/app/Models/user';
 export class UpdateUserComponent implements OnInit {
   id!: number;
   user !:User;
-  updateForm !: FormGroup;
-  constructor(private avtR: ActivatedRoute, private userS : UserService ,private R:Router) { }
-
+  // updateForm !: FormGroup;
+  updateForm = this.FormB.group({
+    id:[0],
+    firstName : ['',Validators.required,Validators.minLength(3)],
+    lastName :[''],
+    birthDate: [''],
+    email: [''],
+    password: [''],
+    profession: [''],
+    accountCategory : [''],
+    picture : [''],
+    skills:this.FormB.array([['',Validators.minLength(3)], ['', Validators.minLength(3)]])
+  })
+  constructor(private avtR: ActivatedRoute, private userS : UserService ,private R:Router , private FormB: FormBuilder) { }
+  
+ 
+  
   ngOnInit(): void {
     this.getParam();
     this.userS.getUserById(this.id).subscribe((data) =>{this.user = data;
-      this.updateForm = new FormGroup({
-        firstName : new FormControl(this.user.firstName),
-        lastName :new FormControl(this.user.lastName),
-        birthDate: new FormControl(this.user.birthDate),
-        email: new FormControl(this.user.birthDate),
-        password: new FormControl(this.user.password),
-        profession: new FormControl(this.user.profession),
-        accountCategory : new FormControl(this.user.accountCategory),
-        picture : new FormControl('')
-      })
+     this.updateForm.setValue(this.user);
     });
   }
 
+  // updateForm = new FormGroup({
+  //   id:new FormControl(this.user.id),
+  //   firstName : new FormControl(this.user.firstName),
+  //   lastName :new FormControl(this.user.lastName),
+  //   birthDate: new FormControl(this.user.birthDate),
+  //   email: new FormControl(this.user.email),
+  //   password: new FormControl(this.user.password),
+  //   profession: new FormControl(this.user.profession),
+  //   accountCategory : new FormControl(this.user.accountCategory),
+  //   picture : new FormControl(''),
+  //   skills:new FormArray([new FormControl(this.user.skills[0],Validators.minLength(3)), new FormControl(this.user.skills[1], Validators.minLength(3))])
+  // })
+get skills(){
+    return this.updateForm.get('skills') as FormArray;
+}
+addskills(){
+    this.skills.push(new FormControl('',Validators.minLength(3)));
+}
   getParam() {
     this.avtR.paramMap.subscribe(data => {this.id = Number(data.get('id'));
-      
+
     });
   }
 
   update(){
-  this.userS.updateUser(this.user).subscribe(
-    ()=> this.R.navigate(['users'])
-    );
+
+
+  this.userS.updateUser(this.user).subscribe(()=> this.R.navigate(['/users']));
   }
 
-  
+
 }
